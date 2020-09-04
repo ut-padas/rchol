bf4d64fc07f642d25b1d4cccd15af97978960466
Chao Chen <chenchao.nk@gmail.com>
2020-09-03 23:09:19 -0500

    minor change

--- matlab/ex_laplace.m
+++ matlab/ex_laplace.m
@@ -13,11 +13,23 @@ G = rchol(A(p,p));
 tol = 1e-6;
 maxit = 50;
 [x, flag, relres, itr] = pcg(A(p,p), b(p), tol, maxit, G, G');
+fprintf('\n')
+fprintf('flag: %d\n', flag)
+fprintf('# iterations: %d\n', itr)
+fprintf('relative residual: %.2e\n', relres)
 
 % verify solution
 y = zeros(length(x), 1);
 y(p) = x;
+fprintf('fill ratio: %.2e\n', 2*nnz(G)/nnz(A))
 fprintf('Relative residual: %.2e\n', norm(b-A*y)/norm(b))
 
 
+Gi = ichol(A, struct('type','ict','droptol',1e-1));
+[x, flag, relres, itr] = pcg(A, b, tol, maxit, Gi, Gi');
+fprintf('\n')
+fprintf('flag: %d\n', flag)
+fprintf('# iterations: %d\n', itr)
+fprintf('fill ratio: %.2e\n', 2*nnz(Gi)/nnz(A))
+fprintf('relative residual: %.2e\n', relres)
 
