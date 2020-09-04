d05e762fa3768f3b5218525d0830c834cd0d190e
Chao Chen <chaochen@login4.frontera.tacc.utexas.edu>
2020-09-04 01:58:55 -0500

    added SDD example

--- matlab/ex_hyperbolic.m
+++ matlab/ex_hyperbolic.m
@@ -1,7 +1,6 @@
 % 3D SDD matrix from hyperbolic equation
 n = 16;
 A = hyperbolic_3d(n);
-%A = hyperbolic_2d(n);
 
 % random RHS
 b = rand(size(A, 1), 1);
@@ -16,8 +15,10 @@ G = rchol(Ae(p,p));
 
 % solve with PCG
 tol = 1e-6;
-maxit = 100;
+maxit = 200;
+tic
 [x, flag, relres, itr] = pcg(Ae(p,p), be(p), tol, maxit, G, G');
+toc
 fprintf('\n')
 fprintf('flag: %d\n', flag)
 fprintf('# iterations: %d\n', itr)
@@ -28,15 +29,5 @@ y = zeros(length(x), 1);
 y(p) = x;
 fprintf('fill ratio: %.2e\n', 2*nnz(G)/nnz(Ae))
 fprintf('Verify residual: %.2e\n', norm(b-A*y(1:length(b)))/norm(b))
-
-
-Gi = ichol(A, struct('type','ict','droptol',1e-1));
-[x, flag, relres, itr] = pcg(A, b, tol, maxit, Gi, Gi');
-fprintf('\n')
-fprintf('flag: %d\n', flag)
-fprintf('# iterations: %d\n', itr)
-fprintf('fill ratio: %.2e\n', 2*nnz(Gi)/nnz(A))
-fprintf('relative residual: %.2e\n', relres)
-
 
 
--- matlab/hyperbolic_2d.m
+++
@@ -1,7 +0,0 @@
-% generate 2d sdd matrix
-function A = hyperbolic_2d(n)
-  e = ones(n, 1);
-  I = speye(n);
-  D = spdiags([-e 2*e -e], [-1 0 1], n, n);
-  A = kron(D, -I) + kron(I, D) + 4*speye(n*n);
-end
