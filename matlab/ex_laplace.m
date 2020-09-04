883ac1fe73822567a079d07788ef4f18a9326f40
Chao Chen <chaochen@login4.frontera.tacc.utexas.edu>
2020-09-04 01:40:23 -0500

    fixed a bug in rchol

--- matlab/ex_laplace.m
+++ matlab/ex_laplace.m
@@ -1,5 +1,5 @@
 % 3D constant Poisson equation with dirichlet boundary
-n = 8;
+n = 128;
 A = laplace_3d(n);
 
 % random RHS
@@ -10,9 +10,12 @@ p = amd(A);
 G = rchol(A(p,p));
 
 % solve with PCG
-tol = 1e-6;
-maxit = 50;
+tol = 1e-10;
+maxit = 200;
+
+tic
 [x, flag, relres, itr] = pcg(A(p,p), b(p), tol, maxit, G, G');
+toc
 fprintf('\n')
 fprintf('flag: %d\n', flag)
 fprintf('# iterations: %d\n', itr)
@@ -23,13 +26,4 @@ y = zeros(length(x), 1);
 y(p) = x;
 fprintf('fill ratio: %.2e\n', 2*nnz(G)/nnz(A))
 fprintf('Relative residual: %.2e\n', norm(b-A*y)/norm(b))
-
-
-Gi = ichol(A, struct('type','ict','droptol',1e-1));
-[x, flag, relres, itr] = pcg(A, b, tol, maxit, Gi, Gi');
-fprintf('\n')
-fprintf('flag: %d\n', flag)
-fprintf('# iterations: %d\n', itr)
-fprintf('fill ratio: %.2e\n', 2*nnz(Gi)/nnz(A))
-fprintf('relative residual: %.2e\n', relres)
 
--- matlab/rchol.m
+++ matlab/rchol.m
@@ -1,8 +1,8 @@
 function G = rchol(A)
     n = size(A,1);
-    L = sddm_to_laplacian(A);
-    edges = -1 * tril(L);
+    Lap = sddm_to_laplacian(A);
+    edges = -1 * tril(Lap);
     nt = 1;
     [D, L] = rchol_lap(edges, [0, n+1], nt);
-    G = L(1:n,1:n).*D(1:n);
+    G = L(1:n,1:n)*spdiags(sqrt(D(1:n)), 0, n, n);
 end
