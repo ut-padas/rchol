9e7f15b4f5bad02a97fad9bc440926e4b19fb1d9
Chao Chen <chaochen@login4.frontera.tacc.utexas.edu>
2020-09-06 01:13:21 -0500

    minor change

--- python/rchol.py
+++ python/rchol.py
@@ -1,12 +1,12 @@
 import numpy as np
 from scipy.sparse import triu, diags
-import python_interface as pyi
-from sddm_to_laplacian import *
+from pyrchol_lap import *
+from util import *
 
 def rchol(A):
   Lap = sddm_to_laplacian(A)
   edges = -1*triu(Lap, format='csr')
-  L, D = pyi.rchol_lap(edges)
+  L, D = rchol_lap(edges)
   Gt = L * diags(np.sqrt(D))
   return Gt.transpose().tocsr()
 
